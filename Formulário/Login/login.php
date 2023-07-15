fb9dee3c39210866028074a8fc9ff5684d028048
Joaovictor23t4 <joaovictor239090@gmail.com>
2023-07-15 00:46:36 -0300

    adicionado-favicons

--- Formulário/Login/login.php
+++ Formulário/Login/login.php
@@ -27,6 +27,7 @@ if (isset($_POST['submit'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style-login.css">
+    <link rel="shortcut icon" href="../controle.ico" type="image/x-icon">
     <title>Tela de Login</title>
 </head>
 <body>
