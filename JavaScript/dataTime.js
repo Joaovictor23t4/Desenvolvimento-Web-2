573fc6d669b7533cbab0a2b34c634275c6c8a657
Joaovictor23t4 <joaovictor239090@gmail.com>
2023-08-19 10:17:15 -0300

    comecando-a-responsividade

--- JavaScript/dataTime.js
+++ JavaScript/dataTime.js
@@ -1,27 +1,32 @@
 function updatePostTime() {
-    const updateTimeElement = document.getElementById("update-time");
+  const updateTimeElements = document.getElementsByClassName("update-time");
+
+  for (const updateTimeElement of updateTimeElements) {
+    const datetimeValue = updateTimeElement.getAttribute("datetime");
     const currentTime = new Date();
-    const updateTimestamp = new Date("2023-08-12T08:33-03:00"); // Substitua pela hora de atualização inicial
-  
-    const timeDiffMinutes = Math.floor((currentTime - updateTimestamp) / (60 * 1000)); // Diferença em minutos
-  
-    if (timeDiffMinutes < 60) {
+    const updateTimestamp = new Date(datetimeValue);
+
+    const timeDiffMinutes = Math.floor((currentTime - updateTimestamp) / (60 * 1000));
+
+    if (timeDiffMinutes === 1) {
+      updateTimeElement.textContent = `há ${timeDiffMinutes} minuto`;
+    }
+     else if (timeDiffMinutes < 60) {
       updateTimeElement.textContent = `há ${timeDiffMinutes} minutos`;
-    } else if (timeDiffMinutes > 60 && timeDiffMinutes < 120) {
-      const timeDiffHours = Math.floor(timeDiffMinutes / 60); // Diferença em horas
-      updateTimeElement.textContent = `há ${timeDiffHours} hora`;
-    } else if (timeDiffMinutes >= 120 && timeDiffMinutes < 1440) {
+    } else if (timeDiffMinutes < 120) {
+      updateTimeElement.textContent = `há 1 hora`;
+    } else if (timeDiffMinutes < 1440) {
       const timeDiffHours = Math.floor(timeDiffMinutes / 60);
       updateTimeElement.textContent = `há ${timeDiffHours} horas`;
-    } else if (timeDiffMinutes >= 1440 && timeDiffMinutes < 2880) {
-      const timeDiffHours = Math.floor(timeDiffMinutes / 1440);
-      updateTimeElement.textContent = `há ${timeDiffHours} dia`;
+    } else if (timeDiffMinutes < 2880) {
+      updateTimeElement.textContent = `há 1 dia`;
     } else {
-      const timeDiffHours = Math.floor(timeDiffMinutes /1440);
-      updateTimeElement.textContent = `há ${timeDiffHours} dias`;
+      const timeDiffDays = Math.floor(timeDiffMinutes / 1440);
+      updateTimeElement.textContent = `há ${timeDiffDays} dias`;
     }
   }
-  
-  // Atualiza a cada minuto
-  setInterval(updatePostTime, 60000);
-  updatePostTime(); // Chama a função inicialmente para exibir o tempo atual
+}
+
+// Atualiza a cada minuto
+setInterval(updatePostTime, 60000);
+updatePostTime(); // Chama a função inicialmente para exibir o tempo atual
